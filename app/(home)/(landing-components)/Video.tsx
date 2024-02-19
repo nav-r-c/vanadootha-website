64ef39e30eeb9e210274661d5d77d96d2522558d
Abhinav Kumar <abhinav.30.kr@gmail.com>
2024-02-19 12:47:43 +0530

    Carousel Component

--- app/(home)/(landing-components)/Video.tsx
+++ app/(home)/(landing-components)/Video.tsx
@@ -4,7 +4,7 @@ function Video() {
     return (
         <>
         <div>
-            <iframe className='mx-auto rounded-md shadow-lg w-[80%]' width="1344" height="515" src="https://www.youtube.com/embed/KJwYBJMSbPI" title="EARTH 4K - Relaxation Film - Peaceful Relaxing Music - Nature 4k Video UltraHD -  OUR PLANET" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe>
+            <iframe className='mx-auto rounded-lg shadow-lg w-[80%]' width="1344" height="515" src="https://www.youtube.com/embed/KJwYBJMSbPI" title="EARTH 4K - Relaxation Film - Peaceful Relaxing Music - Nature 4k Video UltraHD -  OUR PLANET" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe>
         </div>
         </>
     )
