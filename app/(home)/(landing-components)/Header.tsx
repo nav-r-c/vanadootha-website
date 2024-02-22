87547a3e2dc7f00e5c49b1a4a27fb8d6fa6aead1
Abhinav Kumar <abhinav.30.kr@gmail.com>
2024-02-22 23:43:21 +0530

    Added Apostrophe Check

--- app/(home)/(landing-components)/Header.tsx
+++ app/(home)/(landing-components)/Header.tsx
@@ -19,9 +19,9 @@ export default function Header() {
                     whileInView={{ opacity : 1, x : 0 }}
                     viewport={{once : true}}
                 >
-                    <h1 className="text-6xl font-bold tracking-wider leading-none">The Smarter Way To Defend Your Farmland.</h1>
-                    <p className="text-2xl my-10 text-secondary-2">With our smart & autonomous product, you don't have to worry about guarding your fields all night!</p>
-                    <Link href = '#contact-us' className="font-bold bg-primary-2 rounded-full p-2 px-5 text-black text-lg transition-all ease-in-out hover:scale-110">Contact Us</Link>
+                    <h1 className="text-6xl font-bold tracking-wider leading-none">{"The Smarter Way To Defend Your Farmland."}</h1>
+                    <p className="text-2xl my-10 text-secondary-2">{"With our smart & autonomous product, you don't have to worry about guarding your fields all night!"}</p>
+                    <Link href = '#contact-us' className="font-bold bg-primary-2 rounded-full p-2 px-5 text-black text-lg transition-all ease-in-out hover:scale-110">{"Contact Us"}</Link>
                 </motion.div>
             </div>
             <Image src = {HeaderImage} alt = "Product out in the open" width={2000} height={200} className="w-[100%] h-screen" />
