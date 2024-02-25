c028a92ccf624c8d6080421b275c0dcb3577aba7
Abhinav Kumar <abhinav.30.kr@gmail.com>
2024-02-25 16:59:03 +0530

    Added SEO

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -11,6 +11,8 @@ import AnimalSection from "./(landing-components)/AnimalSection";
 import VideoSection from "./(landing-components)/VideoSection";
 import Sponsors from "./(landing-components)/Sponsors";
 
+import Head from "next/head";
+
 export const metadata : Metadata = {
     title : "Vanadootha | The smarter way to defend your farmland",
     description : "“Vanadootha” translates to, “Messenger of the Forest” in the ancient Sanskrit language. All products developed by Vanadootha, will act as a bridge between the wilderness and humanity. It will avoid all the conflicts and clashes between the two sides and will ensure that peace and co-existence thrives, avoiding Eco-Apartheid, leading to an eternal friendship between human beings and the wild. The smarter way to defend your farmland. With our smart & autonomous product, you don't have to worry about guarding your fields all night!"
@@ -19,6 +21,10 @@ export const metadata : Metadata = {
 export default function Page() {
     return (
         <>
+            <Head>
+                <title>Vanadootha | The smarter way to defend your farmland</title>
+                <meta title = "description" content="Vanadootha” translates to, “Messenger of the Forest” in the ancient Sanskrit language. All products developed by Vanadootha, will act as a bridge between the wilderness and humanity. It will avoid all the conflicts and clashes between the two sides and will ensure that peace and co-existence thrives, avoiding Eco-Apartheid, leading to an eternal friendship between human beings and the wild. The smarter way to defend your farmland. With our smart & autonomous product, you don't have to worry about guarding your fields all night!" />
+            </Head>
             <div>
                 <Header />
 
